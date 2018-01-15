c7d8bba222cc7581ec09012f959acd32ef690677
Mikko Rinta-Homi <Mikkojrh@Gmail.com>
2018-01-15 23:42:29 +0200

    Added manual control and fine-tuned the autopilot

--- src/PongDrone.py
+++ src/PongDrone.py
@@ -22,18 +22,59 @@ drone.showVideo()
 
 drone.groundCam()
 
-# We don't want the drone to fly to the roof
+# We don't want the drone to fly to the ceiling
 drone.setConfig("control:altitude max", 1000)
 
 # In case we wan't to save the video...
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter('output.mp4',fourcc, 15.0, (640,360))
 
+font = cv2.FONT_HERSHEY_SIMPLEX
+
 drone.takeoff()
 
-font = cv2.FONT_HERSHEY_SIMPLEX
-IMC = drone.VideoImageCount
+### WAIT AND MANUAL CONTROL ###
+def wait(seconds):
+    manualControl = False
+    mstarget = int(round((time.time() + seconds) * 1000))
+    IMC = drone.VideoImageCount
+    while mstarget > int(round(time.time() * 1000)) or manualControl:
+        # Don't load same frames twice
+        while drone.VideoImageCount == IMC: time.sleep(0.01)
+        IMC = drone.VideoImageCount
+        frame = drone.VideoImage
+        cv2.imshow("Frame", frame)
+        out.write(frame)
 
+        pressedKey = drone.getKey()
+        if pressedKey is 'p':
+            print "DO THE FLIP!!"
+            drone.anim(17, 0.5)
+        elif manualControl:
+            # Manual control settings
+            if pressedKey is 'e': drone.moveForward(0.3)
+            elif pressedKey is 'd': drone.moveBackward(0.3)
+            elif pressedKey is 's': drone.moveLeft(0.3)
+            elif pressedKey is 'f': drone.moveRight(0.3)
+            elif pressedKey is 'w': drone.turnLeft(0.3)
+            elif pressedKey is 'r': drone.turnRight(0.3)
+            elif pressedKey is 't': drone.moveUp(0.3)
+            elif pressedKey is 'g': drone.moveDown(0.3)
+            elif pressedKey is 'n':
+                print "MANUAL CONTROL OFF"
+                manualControl = False
+            else:
+                drone.moveForward(0)
+        elif pressedKey is 'm':
+            print "MANUAL CONTROL ON"
+            manualControl = True
+        # Stop if q is pressed
+        if pressedKey is 'q':
+            return True
+    return False
+
+### PROGRAM AND BRAINS ###
+IMC = drone.VideoImageCount
 while True:
     # Don't load same frames twice
     while drone.VideoImageCount == IMC: time.sleep(0.01)
@@ -55,8 +96,8 @@ while True:
                 targetCircleDistance = np.sqrt(x*x+y*y)
                 targetCircle = x, y
         # Moving according to the circles. Screen size is 640 x 360
-        leftRight = round((targetCircle[0]-320)/320.0/2, 3)
-        backwardForward = -round((targetCircle[1]-180)/180.0/2, 3)
+        leftRight = round((targetCircle[0]-320)/320.0/4, 3)
+        backwardForward = -round((targetCircle[1]-180)/180.0/4, 3)
 
         # Painting the move..
         text = "RIGHT " + str(leftRight) + "%"
@@ -65,19 +106,13 @@ while True:
         cv2.putText(frame, text, (320, 30), font, 1, (255,255,255), 2, cv2.LINE_AA)
 
         drone.move(leftRight, backwardForward, 0, 0)
-        time.sleep(0.1)
-        drone.stop()
-
-    # Paint the frame
-    cv2.imshow("Frame", frame)
-    # Saving the video...
-    out.write(frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('p'):
-        print "DO THE FLIP!!"
-        drone.anim(17, 0.5)
-    # Stop the loop is q is pressed
-    elif cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imshow("Frame", frame)
+        out.write(frame)
+        if wait(abs(leftRight) + abs(backwardForward)):
+            break
+        
+    drone.stop()
+    if wait(0.3):
         break
 
 cv2.destroyAllWindows()
